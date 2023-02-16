b5c88092e59e309894c70224d05711cf3d16b1c0
Yefry Sanchez <yefrymsp@gmail.com>
2023-02-16 17:53:47 -0500

    arrows functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const slides = [
+    {
+      url: "https://images.unsplash.com/photo-1676455205860-a08ff9a5dd07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2325&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1676211986358-8b57ad9a2df0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1676487918369-0e3b85e5240b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1676402519263-54dc34265e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+    },
+  ];
 
+  const [slide, setSlide] = useState(0)
+
+  const slideLeft = () => {
+    if(slide === 0){
+      setSlide(slides.length - 1)
+    } else {
+      setSlide(slide - 1)
+    }
+  }
+
+  const slideRight = () => {
+    if(slide === slides.length - 1){
+      setSlide(0)
+    } else {
+      setSlide(slide + 1)
+    }
+  }
+console.log(slide)
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[slide].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover
+       duration-500"
+      ></div>
+      <div onClick={slideLeft} className="hidden group-hover:block text-slate-100 bg-black/20 rounded-full p-2 absolute top-1/2 translate-y-[-50%] left-5 cursor-pointer">
+        <BsChevronCompactLeft size={30} />
+      </div>
+      <div onClick={slideRight} className="hidden group-hover:block text-slate-100 bg-black/20 rounded-full p-2 absolute top-1/2 translate-y-[-50%] right-5 cursor-pointer">
+        <BsChevronCompactRight size={30} />
+      </div>
     </div>
   );
 }
